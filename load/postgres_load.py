1f020ee970ce578ba2340094cec967f97ad64918
Julian <jmetias@outlook.com>
2026-07-13 07:18:30 +1200

    Fix a bug for not append the rows on the end

--- load/postgres_load.py
+++ load/postgres_load.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 def load_to_postgres(df : pd.DataFrame, table_name: str,schema: str, db_url: str):
     try:
         engine = sa.create_engine(db_url)
-        df.to_sql(table_name, engine,schema = schema, if_exists='append', index=False)
+        df.to_sql(table_name, engine,schema = schema, if_exists='replace', index=False)
         logging.info(f"Loaded {len(df)} rows into {table_name} table")
     except Exception as e:
         logging.error(f"Failed to load data into PostgreSQL: {e}")
