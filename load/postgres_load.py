bdf3fb93fdef64d8923561ca454b78b3f3a35710
Julian <jmetias@outlook.com>
2026-07-14 06:14:11 +1200

    fix postgres volume and fix schema

--- load/postgres_load.py
+++ load/postgres_load.py
@@ -7,9 +7,13 @@ logging.basicConfig(level=logging.INFO)
 def load_to_postgres(df : pd.DataFrame, table_name: str,schema: str, db_url: str):
     try:
         engine = sa.create_engine(db_url)
+        with engine.connect() as connection:
+            logging.info(f"Ensuring schema '{schema}' exists...")
+            connection.execute(sa.text(f"CREATE SCHEMA IF NOT EXISTS {schema};"))
         df.to_sql(table_name, engine,schema = schema, if_exists='replace', index=False)
         logging.info(f"Loaded {len(df)} rows into {table_name} table")
     except Exception as e:
         logging.error(f"Failed to load data into PostgreSQL: {e}")
+        raise e
 
 
